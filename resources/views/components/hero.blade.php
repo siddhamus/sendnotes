3019be95acc286b2af5984fb2a92e9c3e22a90bd
siddhamus <sidhant4499@gmail.com>
2024-05-21 19:15:04 +0530

    Update hero.blade.php

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -6,10 +6,11 @@
                     <div class="max-w-lg">
                         {{-- <img class="h-11" src="{{ asset('img/tok_hero.png') }}" alt="IB Diploma Programme"> --}}
                         <div class="mt-24 sm:mt-32 lg:mt-16">
-                            <a href="#" class="inline-flex space-x-6">
+                            
                                 <span class="px-3 py-1 text-sm font-semibold leading-6 text-indigo-600 rounded-full bg-indigo-600/10 ring-1 ring-inset ring-indigo-600/10">What's new</span>
+                                <a href="#" class="inline-flex space-x-6">
                                 <span class="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600">
-                                    <span>New IBDP Courses Available</span>
+                                    <span>New IBDP English L&L Content</span>
                                     <svg class="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                         <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
                                     </svg>
@@ -25,15 +26,15 @@
                     </div>
                 </div>
             </div>
-            <div class="mt-20 sm:mt-24 md:mx-auto md:max-w-2xl lg:mx-0 lg:mt-0 lg:w-screen">
+            {{-- <div class="mt-20 sm:mt-24 md:mx-auto md:max-w-2xl lg:mx-0 lg:mt-0 lg:w-screen">
                 <div class="absolute inset-y-0 right-1/2 -z-10 -mr-10 w-[200%] skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 md:-mr-20 lg:-mr-36" aria-hidden="true"></div>
                 <div class="shadow-lg md:rounded-3xl">
                     <div class="bg-indigo-500 [clip-path:inset(0)] md:[clip-path:inset(0_round_theme(borderRadius.3xl))]">
                         <div class="absolute -inset-y-px left-1/2 -z-10 ml-10 w-[200%] skew-x-[-30deg] bg-indigo-100 opacity-20 ring-1 ring-inset ring-white md:ml-20 lg:ml-36" aria-hidden="true"></div>
-                        {{-- <img src="{{ asset('img/sc.svg') }}" alt="IBDP Resources"> --}}
+                        <img src="{{ asset('img/sc.svg') }}" alt="IBDP Resources">
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
         <div class="absolute inset-x-0 bottom-0 h-24 -z-10 bg-gradient-to-t from-white sm:h-32"></div>
     </div>
